c000a74ec3b53b680b4c4f4e38cefa2f15bc5fd7
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-05-15 21:01:04 +0330

    Rename bundle to RespinarRedirectionBundle

--- src/RespinarRedirectionBundle.php
+++ src/RespinarRedirectionBundle.php
@@ -10,13 +10,13 @@ declare(strict_types=1);
  * @license MIT
  */
 
-namespace Respinar\ContaoRedirectionBundle;
+namespace Respinar\RedirectionBundle;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class RespinarContaoRedirectionBundle extends AbstractBundle
+class RespinarRedirectionBundle extends AbstractBundle
 {
     public function loadExtension(
         array $config,
