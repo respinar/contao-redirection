b53b1dc66088f6e4da7d9fbd4b2b6db4e6717264
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-05-16 10:31:48 +0330

    Rework redirection DCA field definitions and palettes

--- contao/dca/tl_redirection.php
+++ contao/dca/tl_redirection.php
@@ -23,16 +23,26 @@ $GLOBALS['TL_DCA']['tl_redirection'] = [
             'panelLayout' => 'filter;sort,search,limit',
         ],
         'label' => [
-            'fields' => ['source_url', 'target_url', 'status_code'],
+            'fields' => ['source_url', 'wildcard', 'target_url', 'status_code'],
             'showColumns' => true,
         ],
         'operations' => [
-            'edit' => ['href' => 'act=edit', 'icon' => 'edit.svg'],
-            'delete' => ['href' => 'act=delete', 'icon' => 'delete.svg'],
+            'edit' => [
+                'href' => 'act=edit',
+                'icon' => 'edit.svg',
+            ],
+            'delete' => [
+                'href' => 'act=delete',
+                'icon' => 'delete.svg',
+            ],
         ],
     ],
     'palettes' => [
-        'default' => '{redirect_legend},source_url,target_url,match_type;{settings_legend},status_code,active',
+        '__selector__' => ['status_code'],
+        'default' => '{status_legend},status_code,wildcard;{redirect_legend},source_url,target_url;{settings_legend},active',
+        '301' => '{status_legend},status_code,wildcard;{redirect_legend},source_url,target_url;{settings_legend},active',
+        '302' => '{status_legend},status_code,wildcard;{redirect_legend},source_url,target_url;{settings_legend},active',
+        '410' => '{status_legend},status_code,wildcard;{redirect_legend},source_url;{settings_legend},active',
     ],
     'fields' => [
         'id' => [
@@ -41,39 +51,50 @@ $GLOBALS['TL_DCA']['tl_redirection'] = [
         'tstamp' => [
             'sql' => "int(10) unsigned NOT NULL default '0'",
         ],
-        'source_url' => [
-            'label' => &$GLOBALS['TL_LANG']['tl_redirection']['source_url'],
-            'inputType' => 'text',
-            'eval' => ['mandatory' => true, 'tl_class' => 'w50', 'maxlength' => 255, 'decodeEntities' => true],
-            'sql' => "varchar(255) NOT NULL default ''",
-        ],
-        'target_url' => [
-            'label' => &$GLOBALS['TL_LANG']['tl_redirection']['target_url'],
-            'search' => true,
-            'inputType' => 'text',
-            'eval' => ['decodeEntities' => true, 'maxlength' => 2048, 'dcaPicker' => true, 'tl_class' => 'w50'],
-            'sql' => "varchar(2048) NOT NULL default ''",
-        ],
-        'match_type' => [
-            'label' => &$GLOBALS['TL_LANG']['tl_redirection']['match_type'],
-            'inputType' => 'select',
-            'options' => ['exact', 'wildcard'],
-            'reference' => &$GLOBALS['TL_LANG']['tl_redirection']['match_types'],
-            'eval' => ['tl_class' => 'w50'],
-            'sql' => "varchar(16) NOT NULL default 'exact'",
-        ],
         'status_code' => [
-            'label' => &$GLOBALS['TL_LANG']['tl_redirection']['status_code'],
             'inputType' => 'select',
             'options' => ['301', '302', '410'],
             'reference' => &$GLOBALS['TL_LANG']['tl_redirection']['status_codes'],
-            'eval' => ['tl_class' => 'w50'],
-            'sql' => "varchar(3) NOT NULL default '301'",
+            'eval' => [
+                'submitOnChange' => true,
+                'tl_class' => 'w50',
+            ],
+            'sql' => "varchar(3) NOT NULL default '302'",
+        ],
+        'source_url' => [
+            'inputType' => 'text',
+            'eval' => [
+                'mandatory' => true,
+                'tl_class' => 'w50',
+                'maxlength' => 255,
+                'decodeEntities' => true,
+            ],
+            'sql' => "varchar(255) NOT NULL default ''",
+        ],
+        'wildcard' => [
+            'inputType' => 'checkbox',
+            'eval' => [
+                'tl_class' => 'w50 m12',
+            ],
+            'sql' => "char(1) NOT NULL default ''",
+        ],
+        'target_url' => [
+            'search' => true,
+            'inputType' => 'text',
+            'eval' => [
+                'mandatory' => true,
+                'decodeEntities' => true,
+                'maxlength' => 2048,
+                'dcaPicker' => true,
+                'tl_class' => 'w50',
+            ],
+            'sql' => "varchar(2048) NOT NULL default ''",
         ],
         'active' => [
-            'label' => &$GLOBALS['TL_LANG']['tl_redirection']['active'],
             'inputType' => 'checkbox',
-            'eval' => ['tl_class' => 'w50'],
+            'eval' => [
+                'tl_class' => 'w50 m12',
+            ],
             'sql' => "char(1) NOT NULL default '1'",
         ],
     ],
