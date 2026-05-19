d4404daf179a45f12b371998bb54e5dd0e5ba84a
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-05-19 13:59:15 +0330

    add RedirectRepository

---
+++ src/Repository/RedirectionRepository.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao Redirection Bundle.
+ *
+ * (c) Hamid Peywasti
+ *
+ * @license MIT
+ */
+
+namespace Respinar\RedirectionBundle\Repository;
+
+use Doctrine\DBAL\Connection;
+
+final class RedirectionRepository
+{
+    public function __construct(private readonly Connection $connection)
+    {
+    }
+
+    /**
+     * Returns all published redirections.
+     *
+     * @return array<int, array{
+     *     id: int,
+     *     source_url: string,
+     *     wildcard: bool,
+     *     target_url: string,
+     *     status_code: int
+     * }>
+     */
+    public function findPublished(): array
+    {
+        return $this->connection->fetchAllAssociative(
+            'SELECT id, source_url, wildcard, target_url, status_code
+             FROM tl_redirection
+             WHERE published = ?',
+            [1],
+        );
+    }
+}
