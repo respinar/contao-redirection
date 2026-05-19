a7d2f9428e09f5cd65cf1304e028921b31307ab5
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-05-19 12:44:48 +0330

    convert language strings to xliff format

--- contao/languages/en/default.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-/*
- * Page type (PTY) labels.
- */
-
-$GLOBALS['TL_LANG']['PTY']['error_410'] = ['410 Gone', 'Shows a "410 Gone" message when a redirection rule with status 410 matches.'];
--- contao/languages/en/modules.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-$GLOBALS['TL_LANG']['MOD']['redirection'] = ['Redirection', 'Manage URL redirects (301, 302) and 410 Gone responses.'];
